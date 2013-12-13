d4bb419004b8558ac6657714f6560ea8cc748c70
Adam Ladachowski <adam.ladachowski@gmail.com>
2013-12-13 13:02:10 +0100

    pasting and parsing

--- app/controllers/scan_controller.rb
+++ app/controllers/scan_controller.rb
@@ -1,4 +1,5 @@
 class ScanController < ApplicationController
+  skip_before_filter :verify_authenticity_token  
   def paste
   end
 
@@ -6,5 +7,36 @@ class ScanController < ApplicationController
   end
 
   def add
+  end
+  
+  def parse
+    if ! params[:scan_id]
+      scan=Scan.create
+    else
+      scan=Scan.find(params[:scan_id])
+    end
+    id = scan.id
+    data=params[:paste]
+    data.split("\n").each do |line|
+      line.chomp!
+      scanrow = line.split("\t")
+      if scanrow.length == 6
+        type = Type.where(name: scanrow[3]).first
+        if type.nil?
+          type = Type.create(name: scanrow[3])
+        end
+        type_id = type.id
+        
+        group = Group.where(name: scanrow[2]).first
+        if group.nil?
+          group = Group.create(name: scanrow[2])
+        end
+        group_id = group.id
+        sig_id = scanrow[0]
+        
+        Sig.create(sig_id: scanrow[0], group_id: group_id, type_id: type_id, scan_id: scan.id )
+      end
+    end
+    redirect_to :back
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Sigtool::Application.routes.draw do
   get "scan/paste"
   get "scan/view"
   get "scan/add"
+  post "scan/parse"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
