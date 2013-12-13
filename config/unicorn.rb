4d8d07b1b30c503e1905e21dc98b5ac2678a2a91
Adam Ladachowski <adam.ladachowski@gmail.com>
2013-12-13 21:00:05 +0100

    unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,13 @@
+# Minimal sample configuration file for Unicorn (not Rack) when used
+# with daemonization (unicorn -D) started in your working directory.
+#
+# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
+# documentation.
+# See also http://unicorn.bogomips.org/examples/unicorn.conf.rb for
+# a more verbose configuration using more features.
+
+listen "/home/chi/sigtool/tmp/sockets/unicorn.sock", :backlog => 1024
+
+# listen 2007 # by default Unicorn listens on port 8080
+worker_processes 2 # this should be >= nr_cpus
+pid "/home/chi/sigtool/tmp/pids/unicorn.pid"
